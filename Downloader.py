76f17b495e7815be8961f2a30011d6b4cc275da7
Андрей Васильев <vasilyev@tochka.com>
2023-03-01 17:20:44 +0500

    refactor

--- Downloader.py
+++ Downloader.py
@@ -1,12 +1,21 @@
+import os
+
 import wfdb
+
+from Const import DOWNLOAD_DIRECTORY
 
 
 class Downloader:
     def __init__(self):
         return
 
+    def __hasFile(self, fileName):
+        return os.path.isdir(fileName)
 
     def download(self):
-        wfdb.io.dl_database\
-            (db_dir='mitdb',dl_dir=DOWNLOAD_DIRECTORY,
-            records='all', annotators='all', keep_subdirs=True, overwrite=False);
+        if self.__hasFile(DOWNLOAD_DIRECTORY):
+            print('Files already exists!')
+        else:
+            wfdb.io.dl_database \
+                (db_dir='mitdb', dl_dir=DOWNLOAD_DIRECTORY,
+                 records='all', annotators='all', keep_subdirs=True, overwrite=False)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import os
-import wfdb
-import wfdb.io
+
 from tkinter import *
 from tkinter import ttk, filedialog
+
+from Const import DOWNLOAD_DIRECTORY
 from Downloader import Downloader
+from GraphDrawer import GraphDrawer
 
 # 1 - ЗАГРУЗКА ФАЙЛОВ
 Downloader().download()
@@ -16,42 +17,14 @@ win = Tk();
 win.geometry("700x350");
 
 label = Label(win, text="Click the Button to browse the Files", font=('Georgia 13'));
-
 label.pack(pady=10)
+
+def open_file():
+    # в методе askopenfile задаем начальную директорию и тип файлов для отображения
+    file = filedialog.askopenfile(initialdir=DOWNLOAD_DIRECTORY, mode='r', filetypes=[('Data Files', '*.dat')])
+    GraphDrawer().drawGraph(file)
 
 # добавить кнопку на окно (для открытия диалога выбора файла)
 ttk.Button(win, text="Browse", command=open_file).pack(pady=20)
 
 win.mainloop()
-
-# 3 - ПОЛУЧЕНИЕ ДАННЫХ ДЛЯ ОТРИСОВКИ
-def open_file():
-    # в методе askopenfile задаем начальную директорию и тип файлов для отображения
-    file = filedialog.askopenfile(initialdir=DOWNLOAD_DIRECTORY, mode='r', filetypes=[('Data Files', '*.dat')])
-
-    # если файл был успешно выбран
-    if file:
-        # получаем его абсолютный путь
-        filepath = os.path.abspath(file.name)
-
-        # для построения графика нужен путь к файлу без расширения
-        dataForPlotting = filepath.replace('.dat', '')
-
-	# номер нужен чтобы на графике отображался его номер (3 последнии цифры названия файла)
-      	recordNumber= dataForPlotting[-3:];
-
-	# получить запись
-      	record = wfdb.rdrecord(record_name=dataForPlotting, sampto=3000);
-
-	# получить аннотацию
-      	annotation = wfdb.rdann(record_name=dataForPlotting, extension='atr', sampto=3000);
-
-	# построить график, используя полученные аннотацию и запись
-      	wfdb.plot_wfdb(record=record, annotation=annotation, plot_sym=True,
-      	time_units = 'seconds', title = 'MIT-BIH Record {}'.format(recordNumber),
-      	figsize = (10, 4), ecg_grids = 'all');
-
-	# вывести данные об открытых файлах на панель окна
-      	Label(win, text="The File is located at : " + str(filepath), font=('Aerial 11')).pack();
-
-# 4 - ОТРИСОВКА ГРАФИКА
