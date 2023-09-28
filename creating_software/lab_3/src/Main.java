739be4c6308e84917ebddd2a4182e841f9ca1a0d
Alex <adb6799@gmail.com>
2023-09-28 19:32:03 +0500

    Написал основные методы, которые понадобятся для моделирования. К методам добавлены комментарии.

---
+++ creating_software/lab_3/src/Main.java
@@ -0,0 +1,86 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    /**
+     * Форматировать вывод (2 знака после запятой)
+     */
+    private static String roundTo2(float inputValue) {
+        DecimalFormat df = new DecimalFormat("0.00");
+        return df.format(inputValue);
+
+    }
+
+    /**
+     * Проверка ввода пользователя(+целое число)
+     */
+    private static int getAndCheckInputValueInt(String valueAnnotation) {
+        System.out.println(valueAnnotation);
+        Scanner in = new Scanner(System.in);
+        int userInput = -1;
+        if (in.hasNextInt()) {
+            userInput = in.nextInt();
+            if (userInput < 0) {
+                System.out.println("Число должно быть больше нуля");
+                userInput = -1;
+
+            }
+        } else {
+            System.out.println("Значение должно быть целым числом");
+        }
+
+        return userInput;
+    }
+
+    /**
+     * Проверка ввода пользователя(число с плавающей точкой)
+     */
+    private static float getAndCheckInputValueFloat(String valueAnnotation) {
+        System.out.println(valueAnnotation);
+        Scanner in = new Scanner(System.in);
+        float userInput = -1;
+        if (in.hasNextFloat()) {
+            userInput = in.nextFloat();
+            if (userInput < 0) {
+                System.out.println("Число должно быть больше нуля");
+                userInput = -1;
+
+            }
+        } else {
+            System.out.println("Значение должно быть числом с плавающей точкой");
+        }
+
+        return userInput;
+    }
+
+    /**
+     * Найти логарифм по основанию 2 от числа
+     */
+    private static float log2(float N) {
+        return (float) (Math.log(N) / Math.log(2));
+    }
+
+    /**
+     * Добавить пояснение к результату
+     */
+    private static String annotateResult(String nameOfTheOutput, float result) {
+        return nameOfTheOutput + ": " + roundTo2(result);
+
+    }
+
+    /**
+     * Вывести результат в консоль
+     */
+    private static void printResult(String result) {
+        System.out.println(result);
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+}
